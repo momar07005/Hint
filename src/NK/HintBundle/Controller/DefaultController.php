0b4edd99b51485d0cc4a3d265240ce8d1127618e
momar07005 <papamomarnokho@gmail.com>
2017-07-15 16:06:23 +0000

    beginning with the index page

--- src/NK/HintBundle/Controller/DefaultController.php
+++ src/NK/HintBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('NKHintBundle:Default:index.html.twig');
+        return $this->render('default/index.html.twig');
     }
 }
